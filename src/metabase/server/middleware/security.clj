787e0c6a83b70cc7e956d8cddf9d02b021bfd008
zip <zhipeng.wu@getjerry.com>
2024-05-15 12:11:13 +0800

    fix change dynamic masking service host

--- src/metabase/server/middleware/security.clj
+++ src/metabase/server/middleware/security.clj
@@ -69,7 +69,7 @@
                                     (map (partial format "'sha256-%s'") inline-js-hashes)))
                   :child-src    ["'self'"
                                  ;; TODO - double check that we actually need this for Google Auth
-                                 "https://accounts.google.com https://en.wikipedia.org https://streamlit.ing.getjerry.com https://chatdata.ing.getjerry.com https://chatdata-prod.ing.getjerry.com https://chatdata-dev.ing.getjerry.com http://me.mydomain.com https://dynamic-masking.ing.getjerry.com"]
+                                 "https://accounts.google.com https://en.wikipedia.org https://streamlit.ing.getjerry.com https://chatdata.ing.getjerry.com https://chatdata-prod.ing.getjerry.com https://chatdata-dev.ing.getjerry.com http://me.mydomain.com http://dynamic-masking-service"]
                   :style-src    ["'self'"
                                  "'unsafe-inline'"
                                  "https://accounts.google.com"]
@@ -85,7 +85,7 @@
                                  ;; Metabase Integration
                                  "metabase-proxy.getjerry.com"
                                  ;; Pii masking
-                                 "dynamic-masking.ing.getjerry.com"
+                                 "dynamic-masking-service"
                                  ;; Google analytics
                                  (when (public-settings/anon-tracking-enabled)
                                    "www.google-analytics.com")
